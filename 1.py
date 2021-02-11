d9c9473ce374f215be07c468272b7b7661db169c
revanthn38 <revanthn38@gmail.com>
2021-02-11 11:03:01 +0530

    Added 1.py file

---
+++ 1.py
@@ -0,0 +1,11 @@
+list=[1,2,3,4,6,8]
+total=0
+average=0
+for i in list:
+    total=total+i
+    average=average+(i/len(list))
+print("total marls in six subjects:")
+print(total)
+print("average marks in six subjects:")
+print(average)
+
